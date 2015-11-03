00e1b0d02abb3174301cb54242339cbab87304f4
Matt Sonnhalter <matthew9559@gmail.com>
2015-11-03 04:06:39 -0600

    Added unit tests project and Nunit package

---
+++ Balancing/BalancingTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BalancingTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
